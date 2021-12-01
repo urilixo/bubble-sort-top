70914b8a1866694a2a61a8675b74c4df650e6232
urilixo <falecomelvio@hotmail.com>
2021-12-01 06:56:50 -0300

    Adapted code to follow style guide

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,21 +1,17 @@
-
 def bubble_sort(array)
-    sorted_array = true
-    loop do
-        sorted_array = false
-        for i in (0..array.length-2)
-            if array[i] > array[i+1]
-                array.insert(i+1, array.delete_at(i))
-                array = array.compact
-                sorted_array = true
-            end
-        end
-        unless sorted_array
-            break
-        end
+  loop do
+    sorted_array = false
+    (0..array.length - 2).each do |i|
+      next unless array[i] > array[i + 1]
+
+      array.insert(i + 1, array.delete_at(i))
+      array = array.compact
+      sorted_array = true
     end
-    array
+    break unless sorted_array
+  end
+  array
 end
 
-p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4, 3, 78, 2, 0, 2])
 #=>[0,2,2,3,4,78]
